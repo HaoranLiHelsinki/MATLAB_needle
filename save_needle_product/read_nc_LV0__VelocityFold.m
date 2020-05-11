41277ebc33cd56b2f68d001f99733d0fd6d841d4
Haoran <33316693+HaoranLiHelsinki@users.noreply.github.com>
2020-05-11 11:17:10 +0300

    Update read_nc_LV0__VelocityFold.m

--- save_needle_product/read_nc_LV0__VelocityFold.m
+++ save_needle_product/read_nc_LV0__VelocityFold.m
@@ -1,12 +1,13 @@
-function [current_spec_db_new, velocity] = read_nc_LV0__VelocityFold(current_spec_db, range_offsets, len_alias, specN ,range ,maxVel  )
+function [current_spec_lin_new, velocity] = read_nc_LV0__VelocityFold(current_spec_lin, range_offsets, Ratio_alias, specN ,range ,maxVel  )
 
-% This function is to mitigate the velocity folding when the velocity exceeds Nyquist velocity (maxV)    
+% 1) Observed linear Ze to Normalized linear Ze; 2) Unfold velocity when the velocity exceeds Nyquist velocity (maxV)    
 
     Num_chirp = 1 ;% chirp 1
-
-    temp = current_spec_db(range_offsets(Num_chirp)+1:range_offsets(Num_chirp+1)   , ...
+    len_alias = specN(Num_chirp)*Ratio_alias;
+    factor_ze = 1;
+    temp = current_spec_lin(range_offsets(Num_chirp)+1:range_offsets(Num_chirp+1)   , ...
         specN(Num_chirp)-len_alias+1:specN(Num_chirp) ) ;  
-    current_spec_db_new1 = [temp  current_spec_db(range_offsets(Num_chirp)+1:range_offsets(Num_chirp+1)  ,  ...
+    current_spec_lin_new1 = factor_ze * [temp  current_spec_lin(range_offsets(Num_chirp)+1:range_offsets(Num_chirp+1)  ,  ...
         1: specN(Num_chirp)-len_alias)] ;    
     
     tempv = -maxVel(Num_chirp) : maxVel(Num_chirp)*2/single(specN(Num_chirp)-1) :maxVel(Num_chirp);
@@ -14,29 +15,33 @@ function [current_spec_db_new, velocity] = read_nc_LV0__VelocityFold(current_spe
     velocity.v1 = tempv - d_v *  len_alias;
     
     Num_chirp = 2 ;  % chirp2
-    temp = current_spec_db(range_offsets(Num_chirp)+1:range_offsets(Num_chirp+1), ...
+    len_alias = specN(Num_chirp)*Ratio_alias;
+       factor_ze = 1;
+    temp = current_spec_lin(range_offsets(Num_chirp)+1:range_offsets(Num_chirp+1), ...
         specN(Num_chirp)-len_alias+1:specN(Num_chirp) ) ;   
-    current_spec_db_new2 = [temp  current_spec_db(range_offsets(Num_chirp)+1:range_offsets(Num_chirp+1), ...
+    current_spec_lin_new2 =  factor_ze * [temp  current_spec_lin(range_offsets(Num_chirp)+1:range_offsets(Num_chirp+1), ...
         1: specN(Num_chirp)-len_alias ) ] ;
-    if specN(Num_chirp) < 1024 % patch nan to make the same length
-        current_spec_db_new2(:,specN(Num_chirp)+1:1024) = nan;
+    if specN(Num_chirp) < specN(1) % patch nan to make the same length
+        current_spec_lin_new2(:,specN(Num_chirp)+1:specN(1)) = nan;
     end
     tempv = -maxVel(Num_chirp) : maxVel(Num_chirp)*2/single(specN(Num_chirp)-1) :maxVel(Num_chirp);
     d_v = tempv(2) - tempv(1) ;
     velocity.v2 = tempv - d_v *  len_alias;
     
     Num_chirp = 3 ; % chirp 3
-    temp = current_spec_db(range_offsets(Num_chirp)+1: length(range), ...
+    len_alias = specN(Num_chirp)*Ratio_alias;
+       factor_ze = 1;
+    temp = current_spec_lin(range_offsets(Num_chirp)+1: length(range), ...
         specN(Num_chirp)-len_alias+1:specN(Num_chirp) ) ; 
-    current_spec_db_new3 = [temp  current_spec_db(range_offsets(Num_chirp)+1: length(range),   ...
+    current_spec_lin_new3 =  factor_ze * [temp  current_spec_lin(range_offsets(Num_chirp)+1: length(range),   ...
         1: specN(Num_chirp)-len_alias)] ;   
-    if specN(Num_chirp) < 1024 % patch nan to make the same length
-        current_spec_db_new3(:,specN(Num_chirp)+1:1024) = nan;
+    if specN(Num_chirp) < specN(1) % patch nan to make the same length
+        current_spec_lin_new3(:,specN(Num_chirp)+1:specN(1)) = nan;
     end
     tempv = -maxVel(Num_chirp) : maxVel(Num_chirp)*2/single(specN(Num_chirp)-1) :maxVel(Num_chirp);
     d_v = tempv(2) - tempv(1) ;
     velocity.v3 = tempv - d_v *  len_alias;
     
     
-    current_spec_db_new = [ current_spec_db_new1;  current_spec_db_new2;  current_spec_db_new3];
+    current_spec_lin_new = [ current_spec_lin_new1;  current_spec_lin_new2;  current_spec_lin_new3];
     
